314565f1d961518c048f75bbbef95aed69ec3f14
Dave Hillier <dave.hillier1@gmail.com>
2018-09-20 15:45:36 +0100

    Update setup-agent.ps1

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -3,21 +3,6 @@ param([String]$pat)
 # turn on TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
-# Install Azure Resource Manager Powershell tools
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Force AzureRM
-
-# Install NodeJs
-Invoke-WebRequest https://nodejs.org/dist/v8.12.0/node-v8.12.0-x64.msi -OutFile node.msi
-Start-Process node.msi -ArgumentList '/q' -Wait
-$env:Path += ";" + $Env:ProgramFiles + '\nodejs'
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine )
-
-# Install Git
-Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.19.0.windows.1/Git-2.19.0-32-bit.exe -OutFile Gitinstaller.exe
-Start-Process Gitinstaller.exe -ArgumentList '/VERYSILENT' -Wait
-Remove-Item -Force Gitinstaller.exe
-
 # Install Nuget
 Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile nuget.exe
 Copy-Item nuget.exe -Destination (New-Item $Env:ProgramFiles\nuget -Type container -Force) -Force
@@ -49,15 +34,12 @@ $env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MS
 
 Remove-Item -Force vs_BuildTools.exe;
 
-Invoke-WebRequest -Uri https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe -OutFile BuildTools_Full.exe;
-Start-Process BuildTools_Full.exe -ArgumentList "/NoRestart", "/Silent" -Wait;
 # setup vsts agentcon 
 Invoke-WebRequest -Uri https://vstsagentpackage.azureedge.net/agent/2.140.0/vsts-agent-win-x64-2.140.0.zip -OutFile "$HOME\vsts-agent.zip"
 
-mkdir c:\agent
+mkdir c:\agent # deliberately here for access
 cd c:\agent
 Add-Type -AssemblyName System.IO.Compression.FileSystem 
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\vsts-agent.zip", "$pwd")
 
 .\config.cmd --unattended --url https://guestlinelabs.visualstudio.com --auth pat --token $pat --pool default --agent (hostname) --acceptTeeEula  --runAsService
-# .\config.cmd remove --auth pat --token $pat 
